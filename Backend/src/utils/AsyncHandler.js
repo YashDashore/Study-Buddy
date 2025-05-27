64f5487677a309e220f3723e04758c5072a84070
YashDashore <yash.dashore1@gmail.com>
2025-05-27 17:51:30 +0530

    Updated Async Handler with comments

--- Backend/src/utils/AsyncHandler.js
+++ Backend/src/utils/AsyncHandler.js
@@ -4,6 +4,7 @@
 // AsyncHandler = (func) => { () =>{} }
 // AsyncHandler = (func) => async () => {}
 
+// Wrapper function. Why? --> We have to contact database again and again for Api responses. We can't write the same type of code(async await) again and again. So we create a wrapper function.
 const AsyncHandler = (fn) => async (err, req, res, next) => {
   try {
     await fn(req, res, next);
