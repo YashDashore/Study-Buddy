85fe9c2c7ec666249b53aa1d58b03894fbdfa7a2
YashDashore <yash.dashore1@gmail.com>
2025-05-27 13:59:27 +0530

    Middlewares added

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,9 +1,29 @@
 import express from "express";
-import cors from 'cors';
+import cors from "cors";
 
 const app = express();
-app.get('/',(req,res)=>{
-    res.send("<h1>Home Page</h1>")
-})
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+  })
+);
+
+app.use(
+  express.json({
+    limit: "20kb",
+  })
+); //Accepting the json file.
+
+app.use(
+  express.urlencoded({
+    limit: "20kb",
+  })
+); // Accept data from url and urlencoded helps to decode the data.
+
+app.use(express.static("public")); // used to store pdfs, public assets
+
+app.get("/", (req, res) => {
+  res.send("<h1>Home Page</h1>");
+});
 
 export { app };
