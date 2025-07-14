3cc20894a39da971129c7e9a482952d9c5d46f2f
YashDashore <yash.dashore1@gmail.com>
2025-07-14 12:06:53 +0530

    Update Input.jsx

--- Frontend/src/components/Registration_Login/Input.jsx
+++ Frontend/src/components/Registration_Login/Input.jsx
@@ -1,9 +1,9 @@
 function InputField({ label, name, type, value, onChange }) {
   return (
-    <div className="mb-4">
+    <div className="mb-3">
       <label
         htmlFor={name}
-        className="block text-base font-medium text-gray-700 mb-1"
+        className="block text-sm font-medium text-gray-700 mb-1"
       >
         {label}
       </label>
@@ -13,7 +13,7 @@ function InputField({ label, name, type, value, onChange }) {
         type={type}
         value={value}
         onChange={onChange}
-        className="w-full px-4 py-2 border-2 border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-indigo-500"
+        className="w-full px-3 py-1.5 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-indigo-500"
         required
       />
     </div>
